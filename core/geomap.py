08b8220e75d896c8ac70c89d660e7282f39b066a
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2022-05-10 10:36:41 -0400

    Corrected geomapping issue with input file

--- core/geomap.py
+++ core/geomap.py
@@ -1,16 +1,13 @@
-import gzip
 import json
 import os
-import shutil
-import time
+from ipaddress import IPv4Address
 from pathlib import Path
-
+import colored
 import dns.resolver
-import geoip2.database
 import requests
-from folium import Map, Marker, Popup
+from folium import Map, Marker
 
-from core.utils import DOMAIN, Helpers, logger
+from core.utils import DOMAIN, IP, Helpers, Workers, logger
 
 helpers = Helpers()
 
@@ -23,114 +20,94 @@ if not os.path.exists(geomap_root):
     os.mkdir(geomap_root)
 
 # Working files
-gl_zipped = geomap_root / "GeoLite2-City.mmdb.gz"
-gl_file = geomap_root / "GeoLite2-City.mmdb"
 ip_map_file = os.path.join(geomap_root, "ip_map.html")
+multi_map_file = os.path.join(geomap_root, "multi_map.html")
 
 
-def geo_query_map(qry):
-    # Check if Geolite file exists
-    geolite_check()
-
-    # Used to resolve domains to ip address
+def geo_resolver(qry, whois=None):
     resolver = dns.resolver.Resolver(configure=False)
-    resolver.nameservers = ["1.1.1.1", "8.8.8.8", "8.8.4.4"]
-    if DOMAIN.findall(qry):
-        try:
-            response = resolver.query(qry, "A")
-            qry = response.rrset[-1]
-            map_maxmind(str(qry))
-        except dns.resolver.NoAnswer as err:
-            logger.error(f"[error] {err}")
-    else:
-        map_maxmind(qry)
-
-
-# ---[ GeoLite File Check/Download ]---
-def geolite_check():
-    if os.path.exists(gl_zipped):
-        print(f"{gl_zipped} exists, unzipping...")
-        with gzip.open(gl_zipped, "rb") as f_in:
-            with open(gl_file, "wb") as f_out:
-                shutil.copyfileobj(f_in, f_out)
-            os.remove(gl_zipped)
-
-    if not os.path.exists(gl_file):
-        GEO_URL = "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
-        print("-" * 80)
-        logger.warning(f"[-] {gl_file} does not exist.")
-        geoip_download = input("\n[+] Would you like to download the GeoLite2-City file (yes/no)? ")
-        if geoip_download.lower() == "yes":
-            os.chdir(geomap_root)
-            helpers.download_file(GEO_URL)
-            with gzip.open(gl_zipped, "rb") as f_in, open(gl_file, "wb") as f_out:
-                shutil.copyfileobj(f_in, f_out)
-            os.remove(gl_zipped)
-
-
-# ---[ Geolocate and Map IP Address ]---
-# Ref: https://github.com/maxmind/GeoIP2-python
-def map_maxmind(qry):
+    resolver.nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
     try:
-        geo_reader = geoip2.database.Reader(gl_file)
-        ip_map = Map([40, -5], tiles="OpenStreetMap", zoom_start=3)
-        response = geo_reader.city(qry)
-        if response.location:
-            lat = response.location.latitude
-            lon = response.location.longitude
-            popup = Popup(qry)
-
-            Marker([lat, lon], popup=popup).add_to(ip_map)
-            ip_map.save(ip_map_file)
-    except geoip2.errors.AddressNotFoundError:
-        logger.warning(f"[-] Address {qry} is not in the geoip database.")
-    except FileNotFoundError:
-        logger.info("\n[*] Please download the GeoLite2-City database file: ")
-        print("    --> https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz")
-        time.sleep(2)
+        dns_resp = list(resolver.query(qry, "A"))[-1]
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers):
+        pass
+    else:
+        if whois:
+            workers = Workers(qry)
+            return workers.get_dns_info(dns_resp, qry)
+        return dns_resp
 
 
 def map_free_geo(qry):
-    ip_map = Map([40, -5], tiles="OpenStreetMap", zoom_start=3)
-    freegeoip = f"https://freegeoip.live/json/{qry}"
-    try:
-        req = requests.get(freegeoip)
-        req.raise_for_status()
-    except ConnectionError as err:
-        logger.warning(f"[error] {err}\n")
-    else:
-        if req.status_code == 200:
-            data = json.loads(req.content.decode("utf-8"))
-            lat = data["latitude"]
-            lon = data["longitude"]
-            Marker([lat, lon], popup=qry).add_to(ip_map)
-            ip_map.save(ip_map_file)
+    if DOMAIN.findall(qry):
+        qry = geo_resolver(qry)
+
+    if qry is not None:
+        ip_map = Map([40, -5], tiles="OpenStreetMap", zoom_start=3)
+        freegeoip = f"https://freegeoip.live/json/{qry}"
+        try:
+            req = requests.get(freegeoip)
+            req.raise_for_status()
+        except ConnectionError as err:
+            logger.warning(f"[error] {err}\n")
+        else:
+            if req.status_code == 200:
+                data = json.loads(req.content.decode("utf-8"))
+                lat = data["latitude"]
+                lon = data["longitude"]
+                Marker([lat, lon], popup=qry).add_to(ip_map)
+                ip_map.save(ip_map_file)
 
 
 def multi_map(input_file):
     os.chdir(geomap_root)
-
-    # Check if Geolite file exists
-    geolite_check()
-
     file_path = os.path.abspath(os.pardir)
     input_file = f"{file_path}/{input_file}"
+    ip_map = Map([40, -5], tiles="OpenStreetMap", zoom_start=3)
+
     with open(input_file, encoding="utf-8") as file_obj:
         line = [line.strip() for line in file_obj.readlines()]
-        ip_map = Map([40, -5], tiles="OpenStreetMap", zoom_start=3)
-        try:
-            geo_reader = geoip2.database.Reader("GeoLite2-City.mmdb")
-            for address in line:
-                response = geo_reader.city(address)
-                if response.location:
+        for address in line:
+            if DOMAIN.findall(address):
+                workers = Workers(address)
+                print(colored.stylize("\n--[ Querying Domain Blacklists ]--", colored.attr("bold")))
+                workers.spamhaus_dbl_worker()
+                workers.blacklist_dbl_worker()
+                if workers.whois_query(address):
+                    print(workers.whois_query(address))
+
+                qry = geo_resolver(address)
+                if qry is not None:
+                    logger.success(f"[+] Mapping {address} -> {qry}")
+                    try:
+                        freegeoip = f"https://freegeoip.live/json/{qry}"
+                        req = requests.get(freegeoip)
+                        req.raise_for_status()
+                    except ConnectionError as err:
+                        logger.warning(f"[error] {err}\n")
+                    else:
+                        data = json.loads(req.content.decode("utf-8"))
+                        lat = data["latitude"]
+                        lon = data["longitude"]
+                        html = f"""{address}<br>
+                        {qry}"""
+                        Marker([lat, lon], popup=html).add_to(ip_map)
+                        ip_map.save(multi_map_file)
+
+            if IP.findall(address) and IPv4Address(address):
+                workers = Workers(address)
+                workers.query_ip(address)
+                if address is not None:
                     logger.success(f"[+] Mapping {address}")
-                    lat = response.location.latitude
-                    lon = response.location.longitude
-                    Marker([lat, lon], popup=address).add_to(ip_map)
-                    ip_map.save("multi_map.html")
-        except ValueError as err:
-            print(f"[error] {err}")
-        except geoip2.errors.AddressNotFoundError:
-            logger.warning("[-] Address is not in the geoip database.")
-        except FileNotFoundError:
-            geolite_check()
+                    try:
+                        freegeoip = f"https://freegeoip.live/json/{address}"
+                        req = requests.get(freegeoip)
+                        req.raise_for_status()
+                    except ConnectionError as err:
+                        logger.warning(f"[error] {err}\n")
+                    else:
+                        data = json.loads(req.content.decode("utf-8"))
+                        lat = data["latitude"]
+                        lon = data["longitude"]
+                        Marker([lat, lon], popup=address).add_to(ip_map)
+                        ip_map.save(multi_map_file)
