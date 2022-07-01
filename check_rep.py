4044768126cdbbeb78fb2e9514e6257d72f976ed
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2022-07-01 08:43:32 -0400

    Updated Python version requirement

--- check_rep.py
+++ check_rep.py
@@ -205,7 +205,7 @@ if __name__ == "__main__":
 
     # ---[ Python v3.7+ check ]---
     if sys.version_info[0] == 3 and sys.version_info[1] <= 8:
-        sys.exit("\n[x] Please use python version 3.8 or higher.\n")
+        print("\n[x] Please use python version 3.8 or higher.\n")
 
     # ---[ Configuration Parser ]---
     yaml = YAML()
